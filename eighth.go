0fcd68a188c5f4aa8859153fa2a69a20f6a12c2f
Hashim <113883060+Hashim-777x@users.noreply.github.com>
2026-01-11 14:26:43 +0530

    last tour of go!

---
+++ eighth.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // send sum to channel c
+}
+
+func main() {
+	fmt.Println(">> DAY 8: CONCURRENCY & CHANNELS")
+
+	s := []int{7, 2, 8, -9, 4, 0}
+	c := make(chan int)
+
+	// Start two goroutines to split the work
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+
+	// Receive the results from the channel
+	x, y := <-c, <-c
+
+	fmt.Println("Result X, Y, and Total:", x, y, x+y)
+}
